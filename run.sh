7721870ed5b43da672696b70aa53c3c2d481deb0
Raphael Huefner <raphaelhuefner@gmail.com>
2025-11-27 11:46:36 +0100

    Allow run.sh to be called from any other working dir

--- run.sh
+++ run.sh
@@ -2,6 +2,10 @@
 
 set -o nounset -o pipefail -o errexit
 
+# Allow this script to be run from any other working directory.
+pushd "$(dirname "$0")" > /dev/null
+trap "popd > /dev/null" EXIT
+
 python3 serve.py \
     --port 8080 \
     --bind 127.0.0.1 \
